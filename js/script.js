bdb76744b0733ec779460fcb92169736e9709d18
Davide Lugli <luglidavide.web@gmail.com>
2020-01-08 16:21:50 +0100

    Fine

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert('weee');
+// lista Cognomi
+var listaCognomi = ['Kubril', 'Taranvino', 'Scozzese', 'Noban'];
+console.log(listaCognomi);
+// Chiedo all’utente il cognome,
+var cognomeUtente = prompt('Inserisci il tuo cognome').toUpperCase();
+while (cognomeUtente.length <= 0) {
+  var cognomeUtente = prompt('Inserisci il tuo cognome').toUpperCase();
+}
+console.log(cognomeUtente);
+// aggiungo cognome utente alla lista
+listaCognomi.push(cognomeUtente);
+console.log(listaCognomi);
+// ordino lista alfabeticamente
+listaCognomi.sort();
+for (var i = 0; i < listaCognomi.length; i++) {
+  document.getElementById('lista_cognomi').innerHTML += '<li>' + listaCognomi[i] + '</li>';
+}
+// posizione del cognome utente nella lista
+var posizioneCognome = listaCognomi.indexOf(cognomeUtente) + 1;
+document.getElementById('posizione').innerHTML = posizioneCognome;
